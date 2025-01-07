50b448388cb3bde02616187c916b587457c900b2
David Reynolds <dr14598@my.bristol.ac.uk>
2025-01-07 09:27:27 +0000

    Customised admin

--- events/admin.py
+++ events/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Event
 
 # Register your models here.
-admin.site.register(Event)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'teacher',
+    )
+
+    ordering = ('name',)
+
+admin.site.register(Event, EventAdmin)
