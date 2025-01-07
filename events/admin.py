144f8e3ca80f89ad42a579329cb7a3818dd20948
David Reynolds <dr14598@my.bristol.ac.uk>
2025-01-07 09:55:20 +0000

    Added events views and templates

--- events/admin.py
+++ events/admin.py
@@ -5,10 +5,11 @@ from .models import Event
 
 class EventAdmin(admin.ModelAdmin):
     list_display = (
+        'date',
         'name',
         'teacher',
     )
 
-    ordering = ('name',)
+    ordering = ('date',)
 
 admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Event(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     teacher = models.CharField(max_length=254)
+    date = models.DateField(default=datetime.date.today)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
