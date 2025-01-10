fd279c02f8624df2e4d6e1fda8fbb4fc8b8534a0
David Reynolds <dr14598@my.bristol.ac.uk>
2025-01-10 16:29:11 +0000

    Added event details functionality

--- events/views.py
+++ events/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Event
 
 # Create your views here.
+
 
 def all_events(request):
     """ A view to show all events, including sorting """
@@ -13,3 +14,15 @@ def all_events(request):
     }
 
     return render(request, 'events/events.html', context)
+
+
+def event_detail(request, event_id):
+    """ A view to show individual product details """
+
+    event = get_object_or_404(Event, pk=event_id)
+
+    context = {
+        'event': event,
+    }
+
+    return render(request, 'events/event_detail.html', context)
